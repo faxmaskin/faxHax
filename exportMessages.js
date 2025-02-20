a939d8eb934b227d93e008f2f5e735ca23febe56
faxmaskin <45529555+faxmaskin@users.noreply.github.com>
2025-02-20 07:55:57 +0100

    updated const imgURL

--- exportMessages.js
+++ exportMessages.js
@@ -1,5 +1,5 @@
 /*
-exportMessages - exportera alla Ã¤rendets meddelanden och interna noteringar till pdf-filer
+exportMessages - exportera alla ärendets meddelanden och interna noteringar till pdf-filer
 https://github.com/faxmaskin/faxHax
 */
 //#region Licens
@@ -29,7 +29,12 @@ SOFTWARE.
 //#endregion
 
 //#region KONSTANTER OCH VARIABLER
-const imgURL = "/administration/profiler/file/2/pdf.flowinstance.logofile/pdf-logo.png";
+
+/*
+OBS! 
+Kontrollera sökvägen till din pdf-logo (const imgURL nedan), i mitt fall är det profil 2, vanligtvis är det 1
+*/
+const imgURL = "/administration/profiler/file/1/pdf.flowinstance.logofile/pdf-logo.png";
 const tjanst = document.querySelector(".title").querySelector('h1').textContent.trim();
 const errandno = document.querySelector(".title").querySelector('span').textContent.trim();
 const arende = `#${errandno.split(":")[1].trim()}`;
@@ -38,28 +43,28 @@ let inskickat = "";
 const historyDiv = document.querySelector('#history');
 const rows = historyDiv.querySelectorAll('table.oep-table tbody tr');
 for (const row of rows) {
-    const eventCell = row.querySelector('td[data-title="HÃ¤ndelse"]');
+    const eventCell = row.querySelector('td[data-title="Händelse"]');
     if (eventCell && eventCell.textContent.trim() === "Inskickat") {
         inskickatAv = row.querySelector('td[data-title="Person"]').textContent.trim();
         inskickat = row.querySelector('td[data-title="Datum"]').textContent.trim();
     }
 }
 //#endregion
-//#region FÃ–RBREDELSER I DOM
-// LÃ¤gg till script fÃ¶r att anvÃ¤nda jsPDF
+//#region FÖRBREDELSER I DOM
+// Lägg till script för att använda jsPDF
 const jspdf = document.createElement("script");
 jspdf.src = "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js";
 document.head.appendChild(jspdf);
 
-// LÃ¤gg till knappar fÃ¶r att hÃ¤mta meddelanden och interna noteringar
+// Lägg till knappar för att hämta meddelanden och interna noteringar
 let btn = document.createElement("button");
 btn.id = "generateMsgPDF";
-btn.innerHTML = "HÃ¤mta meddelanden";
+btn.innerHTML = "Hämta meddelanden";
 btn.classList.add("btn", "btn-green", "btn-inline", "bigmarginright");
 document.querySelector(".editbutton").append(btn);
 btn = document.createElement("button");
 btn.id = "generateNotePDF";
-btn.innerHTML = "HÃ¤mta interna noteringar";
+btn.innerHTML = "Hämta interna noteringar";
 btn.classList.add("btn", "btn-green", "btn-inline", "bigmarginright");
 document.querySelector(".editbutton").append(btn);
 //#endregion
@@ -83,34 +88,34 @@ async function createPDF(id, title, arende, inskickatAv, inskickat, tjanst, imgU
     const reader = new FileReader();
     reader.onloadend = function () {
         const base64data = reader.result.split(",")[1];
-        // Ã–versta raden (logotyp, datumrubrik, Ã¤rendenummerrubrik och sida och sidoantal)
+        // Översta raden (logotyp, datumrubrik, ärendenummerrubrik och sida och sidoantal)
         doc.addImage(base64data, "PNG", 17.97, y, 31.75, 12.96, 180);
         doc.setFontSize(7.5);
         doc.setFont("Helvetica");
         doc.text("Datum", 117.47, y);
-        doc.text("Ã„rendenummer", 144.03, y);
+        doc.text("Ärendenummer", 144.03, y);
         y += 3.18;
-        // Andra raden (datum och Ã¤rendenummer)
+        // Andra raden (datum och ärendenummer)
         doc.text(getCurrentDate(), 117.47, y);
         doc.text(arende, 144.03, y);
-        // FjÃ¤rde raden (inskickat av)
+        // Fjärde raden (inskickat av)
         y += 3.18 * 2;
         doc.text(`Inskickat av: ${inskickatAv}`, 117.47, y);
         doc.setFontSize(10.5, 'bold');
         // Nioende raden (rubrik)
         y += 3.18 * 5;
-        doc.text(`Samtliga ${title} fÃ¶r Ã¤rendenummer ${arende}, ${tjanst}`, 17.97, y);
-        // FÃ¶rbered fÃ¶r meddelanden
+        doc.text(`Samtliga ${title} för ärendenummer ${arende}, ${tjanst}`, 17.97, y);
+        // Förbered för meddelanden
         y += 3.18 * 2.5;
         doc.setDrawColor(0);
         doc.setFontSize(7.5);
         doc.setFont("Times-Roman");
-        // FÃ¶rsta sidfot
+        // Första sidfot
         doc.line(15.89, 283.93, 189, 283.93, "S");
-        doc.text(`Ã„rendenummer: ${arende} | Inskickat av: ${inskickatAv.split('(')[0].trim()} | Datum: ${inskickat}`, 15.89, 288.7);
+        doc.text(`Ärendenummer: ${arende} | Inskickat av: ${inskickatAv.split('(')[0].trim()} | Datum: ${inskickat}`, 15.89, 288.7);
         doc.setDrawColor(150);
 
-        // HÃ¤mta alla meddelanden eller interna noteringar
+        // Hämta alla meddelanden eller interna noteringar
         let messages = document.querySelectorAll(`#${id} .messages > li`);
 
         // Loopa igenom alla meddelanden/noteringar
@@ -118,28 +123,28 @@ async function createPDF(id, title, arende, inskickatAv, inskickat, tjanst, imgU
             let authorText = msg.querySelector(".author")?.innerText.trim() || "";
             let messageText = msg.querySelector(".message div:first-child")?.innerText.trim() || "";
 
-            // Kontrollera om ny sida behÃ¶vs
+            // Kontrollera om ny sida behövs
             if (y > 260) {
                 doc.addPage();
                 doc.setDrawColor(0);
                 doc.line(15.89, 283.93, 189, 283.93, "S");
-                doc.text(`Ã„rendenummer: ${arende} | Inskickat av: ${inskickatAv.split('(')[0].trim()} | Datum: ${inskickat}`, 15.89, 288.7);
+                doc.text(`Ärendenummer: ${arende} | Inskickat av: ${inskickatAv.split('(')[0].trim()} | Datum: ${inskickat}`, 15.89, 288.7);
                 y = 18.81;
                 doc.setDrawColor(150);
             }
-            // LÃ¤gg till fÃ¶rfattare
+            // Lägg till författare
             doc.setFontSize(7.5);
             doc.setFont("Helvetica", "bold");
             doc.text(`# ${index + 1}`, 17.97, y, { maxWidth: 180 });
             doc.setFont("Helvetica", "normal");
             y += 3.18;
-            doc.text(`AvsÃ¤ndare: ${authorText.split('Â·')[0].trim()} | Skickat: ${authorText.split('Â·')[1].split('Skapad:')[1].trim()}`, 17.97, y, { maxWidth: 180 });
+            doc.text(`Avsändare: ${authorText.split('·')[0].trim()} | Skickat: ${authorText.split('·')[1].split('Skapad:')[1].trim()}`, 17.97, y, { maxWidth: 180 });
 
-            // Dela upp meddelandetexten om det Ã¤r fÃ¶r lÃ¥ngt
+            // Dela upp meddelandetexten om det är för långt
             let messageLines = doc.splitTextToSize(messageText, 180);
             let lineHeight = 3.18;
 
-            // LÃ¤gg till meddelandetext i PDF
+            // Lägg till meddelandetext i PDF
             y += 3.18 * 1.5;
             let ry = y - 3.18;
             doc.setFontSize(7.5);
@@ -149,7 +154,7 @@ async function createPDF(id, title, arende, inskickatAv, inskickat, tjanst, imgU
             y += messageLines.length * lineHeight + 10;
         });
 
-        // LÃ¤gg till sidnummer i sidhuvudet
+        // Lägg till sidnummer i sidhuvudet
         const addPageNumbers = () => {
             const totalPages = doc.internal.getNumberOfPages();
 
@@ -168,7 +173,7 @@ async function createPDF(id, title, arende, inskickatAv, inskickat, tjanst, imgU
     reader.readAsDataURL(blob);
 }
 //#endregion
-//#region 2.0 KÃ–R SCRIPTET
+//#region 2.0 KÖR SCRIPTET
 if (window.location.href.includes('/manage/flowinstanceadmin/overview/')) {
     document.querySelector("#generateMsgPDF").addEventListener("click", async function () {
         createPDF("messages", "meddelanden", arende, inskickatAv, inskickat, tjanst, imgURL);
